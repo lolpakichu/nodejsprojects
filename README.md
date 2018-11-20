1f9d31cc5dcd625d10dfcfb148c4318dfb17f569
pakichu <smolguppy@gmail.com>
2018-11-20 08:39:43 -0600

    new file system demos

---
+++ README.md
@@ -0,0 +1,2 @@
+# NodeJS Demos
+These are demos I am currently using to learn Node.js and Javascript. I learned how to code these demos from [here](https://www.w3schools.com/nodejs/default.asp)
--- hellonode.js
+++ hellonode.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var dt = require('./firstmodule');
-var fs = require('fs');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
